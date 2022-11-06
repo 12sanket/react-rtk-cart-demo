8541c194232bf8c0a821118ad05d9892b419c4e3
Sanket <12sanketkumbhar@gmail.com>
2022-11-06 17:37:16 +0530

    add, remove, calculate total functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { calculateTotal} from './features/cart/cartSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from "react";
+
 function App() {
+  const { cartItems } = useSelector((store) => store.cart)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(calculateTotal())
+  }, [cartItems])
   return (
   <main>
     <Navbar></Navbar>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,10 +14,32 @@ const cartSlice = createSlice({
     reducers: {
         clearCart : (state) => {
             state.cartItems = []
+        },
+        removeItem: (state, {payload}) => {
+            console.log('remove');
+            state.cartItems = state.cartItems.filter((item) => item.id !== payload.id )
+        },
+        increase : (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id)
+            cartItem.amount += 1 
+        },
+        decrease : (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id)
+            cartItem.amount -= 1 
+        },
+        calculateTotal : (state) => {
+            let amount = 0
+            let total = 0
+            state.cartItems.forEach(item => {
+                amount += item.amount
+                total += item.amount * item.price
+            });
+            state.amount = amount
+            state.total = total
         }
     }
 })
 
 //console.log(cartSlice);
-export const { clearCart } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease, calculateTotal} = cartSlice.actions
 export default cartSlice.reducer;
